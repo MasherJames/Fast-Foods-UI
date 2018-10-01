57830349a2b95259e791cdc621e8b5ad5807cc96
James Macharia <jamesmasher13@gmail.com>
2018-10-01 16:47:56 +0300

    Add all

--- src/admin-item.js
+++ src/admin-item.js
@@ -1,4 +1,13 @@
-let itemContainer = document.querySelector(".fooditem-container");
-itemContainer.addEventListener("click", e => {
-  e.preventDefault();
-});
+// let itemContainer = document.querySelector(".fooditem-container");
+// itemContainer.addEventListener("click", e => {
+//   e.preventDefault();
+//   let token = window.localStorage.getItem("token");
+
+//   fetch("https://foods-fasty.herokuapp.com/api/v1/fooditems", {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json",
+//       Authorization: `Bearer ${token}`
+//     }
+//   });
+// });
--- src/all-fooditems.js
+++ src/all-fooditems.js
@@ -10,21 +10,39 @@ window.onload = () => {
   })
     .then(res => res.json())
     .then(data => {
-      console.log(data["Food Items"]);
       let output = "";
-      data["Food Items"].forEach(request => {
+      data["Food Items"].forEach(item => {
+        let itemContainer = document.querySelector(".fooditem-container");
+        itemContainer.addEventListener("click", () => {
+          e.preventDefault();
+        });
         output += `
            <div class="fooditem-container">
-                    <span class="food-name">${request["name"]}</span>
-                    <span class="date">${request["date"]}</span>
+                    <span class="food-name">${item["name"]}</span>
+                    <span class="date">${item["date"]}</span>
                     <div class="food-desc-img">
                         <img src="../img/food2.jpg" alt="food-photo" class="food-image">
-                        <p class="food-desc">${request["description"]}</p>
+                        <p class="food-desc">${item["description"]}</p>
                     </div>
-                    <h5 class="food-price">ksh. ${request["price"]}</h5>
+                    <h5 class="food-price">ksh. ${item["price"]}</h5>
                 </div>
         `;
       });
       document.querySelector(".admin-items").innerHTML = output;
+    });
+};
+
+const viewFood = () => {
+  fetch(`https://foods-fasty.herokuapp.com/api/v1/fooditems/${request["id"]}`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    }
+  })
+    .then(res => res.json())
+    .then(data => {
+      localStorage.setItem("food_id", data["id"]);
+      window.location.assign("fooditem.html");
     });
 };
