fbd87f9c679debbe396de651f8e04b725209cd28
James Macharia <jamesmasher13@gmail.com>
2018-10-03 17:57:36 +0300

    Update admin dashboard

--- src/all-fooditems.js
+++ src/all-fooditems.js
@@ -1,3 +1,22 @@
+const renderFoodItem = foodItem => {
+  const markup = `
+    <div class="fooditem-container">
+          <a href="#${foodItem["id"]}" class="fooditem-container__link">
+              <span class="food-name">${foodItem["name"]}</span>
+              <span class="date">${foodItem["date"]}</span>
+              <div class="food-desc-img">
+                  <img src="../img/gg.jpg" alt="food-photo" class="food-image">
+                  <p class="food-desc">${foodItem["description"]}</p>
+              </div>
+              <h5 class="food-price">ksh. ${foodItem["price"]}</h5>
+          </a>
+    </div>
+  `;
+  document
+    .querySelector(".admin-items")
+    .insertAdjacentHTML("beforeend", markup);
+};
+
 window.onload = () => {
   let token = window.localStorage.getItem("token");
 
@@ -10,39 +29,6 @@ window.onload = () => {
   })
     .then(res => res.json())
     .then(data => {
-      let output = "";
-      data["Food Items"].forEach(item => {
-        let itemContainer = document.querySelector(".fooditem-container");
-        itemContainer.addEventListener("click", () => {
-          e.preventDefault();
-        });
-        output += `
-           <div class="fooditem-container">
-                    <span class="food-name">${item["name"]}</span>
-                    <span class="date">${item["date"]}</span>
-                    <div class="food-desc-img">
-                        <img src="../img/food2.jpg" alt="food-photo" class="food-image">
-                        <p class="food-desc">${item["description"]}</p>
-                    </div>
-                    <h5 class="food-price">ksh. ${item["price"]}</h5>
-                </div>
-        `;
-      });
-      document.querySelector(".admin-items").innerHTML = output;
-    });
-};
-
-const viewFood = () => {
-  fetch(`https://foods-fasty.herokuapp.com/api/v1/fooditems/${request["id"]}`, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`
-    }
-  })
-    .then(res => res.json())
-    .then(data => {
-      localStorage.setItem("food_id", data["id"]);
-      window.location.assign("fooditem.html");
+      data["Food Items"].forEach(renderFoodItem);
     });
 };
